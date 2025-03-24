4c6ae2370ae595bd69389a512688db594fe8d8c4
shreyasonii28 <shreyasoniii28@gmail.com>
2025-03-24 23:38:21 +0530

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,9 @@
+numbers = list(range(1, 11))
+
+extracted = numbers[:5]
+
+reversed_extracted = extracted[::-1]
+
+print("Original list:", numbers)
+print("Extracted first five elements:", extracted)
+print("Reversed extracted elements:", reversed_extracted)
